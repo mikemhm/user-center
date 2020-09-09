1ff6ebb9816cdae8f37dfd200c515284940f4d84
mike <mikemhm@126.com>
2020-09-09 16:10:11 +0800

    添加 bonusEvent

---
+++ src/main/java/com/itmuch/usercenter/domain/entity/user/BonusEventLog.java
@@ -0,0 +1,51 @@
+package com.itmuch.usercenter.domain.entity.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BonusEventLog {
+    /**
+     * Id
+     */
+    @Id
+    @GeneratedValue(generator = "JDBC")
+    private Integer id;
+
+    /**
+     * user.id
+     */
+    @Column(name = "user_id")
+    private Integer userId;
+
+    /**
+     * 积分操作值
+     */
+    private Integer value;
+
+    /**
+     * 发生的事件
+     */
+    private String event;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "create_time")
+    private Date createTime;
+
+    /**
+     * 描述
+     */
+    private String description;
+}
